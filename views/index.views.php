83e94eb2096e3cfc87999b93be6e00f48304417b
Haemoffy <fuwansuu@gmail.com>
2024-11-20 23:38:29 +0800

    content for home page

--- views/index.views.php
+++ views/index.views.php
@@ -1,3 +1,10 @@
 <?php require 'partials/head.php' ?>
-
+<div class="home-cont">
+    <h1 class="title">NightShade~</h1>
+    <p class="desc">A simple ToDo website for casual ToDo enjoyers.</p>
+    <div class="user-admission-btns-cont">
+        <a href="login" class="login-btn" type="button">Login</a>
+        <a href="/register" class="register-btn" type="button">Register</a>
+    </div>
+</div>
 <?php require 'partials/footer.php' ?>
