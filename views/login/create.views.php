ef1a1b1dc5f7c9a46a26ea6d8b11923eed391c19
Haemoffy <fuwansuu@gmail.com>
2024-11-20 23:38:40 +0800

    content fo login page

--- views/login/create.views.php
+++ views/login/create.views.php
@@ -1,3 +1,19 @@
 <?php require ROOT . 'views/partials/head.php' ?>
-
+<div class="login-cont">
+    <h1>Login~</h1>
+    <form class="login-form" method="post">
+        <div class="input-cont">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div class="input-cont">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="pwd">
+        </div>
+        <div class="submit-btn">
+            <button type="submit">Login</button>
+        </div>
+    </form>
+    <!-- add text that return to page to directly register -->
+</div>
 <?php require ROOT . 'views/partials/footer.php' ?>
