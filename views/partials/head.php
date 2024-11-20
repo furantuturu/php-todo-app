9ffe1ca3cb977dc25898544ba488d2b116586b4f
Haemoffy <fuwansuu@gmail.com>
2024-11-20 23:39:03 +0800

    csp for css and added body layour var

--- views/partials/head.php
+++ views/partials/head.php
@@ -6,9 +6,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <!-- https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP -->
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self';">
+    <meta http-equiv="Content-Security-Policy"
+        content="default-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com/; font-src 'self' https://fonts.gstatic.com/">
     <link rel="stylesheet" href="static/css/styles.css">
 
 </head>
 
-<body class="bg-design <?= $bg ?>">
+<body class="bg-design <?= $bg ?> <?= $bodyLayout ?>">
