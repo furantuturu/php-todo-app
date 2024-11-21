428225eaf045414478cbd280fb2d7aca18762566
Haemoffy <fuwansuu@gmail.com>
2024-11-21 23:11:09 +0800

    added dotenv and session

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
+use Dotenv\Dotenv;
+session_start();
+
 const ROOT = __DIR__ . '/../';
 
 require ROOT . "vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable(ROOT);
+$dotenv->load();
 
 require ROOT . "helpers.php";
 
@@ -14,3 +20,5 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
+
+Classes\Session::unflash();
