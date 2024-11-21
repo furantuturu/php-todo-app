f15c71a60fa48f4e575a07c7611e010accb6f63f
Haemoffy <fuwansuu@gmail.com>
2024-11-21 23:12:12 +0800

    updated content for login and created content for register

--- views/login/create.views.php
+++ views/login/create.views.php
@@ -4,16 +4,19 @@
     <form class="login-form" method="post">
         <div class="input-cont">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" required>
         </div>
         <div class="input-cont">
             <label for="password">Password</label>
-            <input type="password" id="password" name="pwd">
+            <input type="password" id="password" name="pwd" required>
         </div>
         <div class="submit-btn">
             <button type="submit">Login</button>
         </div>
     </form>
-    <!-- add text that return to page to directly register -->
+    <nav class="page-links">
+        <a href="/">Return to Home</a>
+        <a href="/register">Register</a>
+    </nav>
 </div>
 <?php require ROOT . 'views/partials/footer.php' ?>
--- views/register/create.views.php
+++ views/register/create.views.php
@@ -1,3 +1,21 @@
 <?php require ROOT . 'views/partials/head.php' ?>
-
-<?php require ROOT . 'views/partials/footer.php' ?>
+<div class="register-cont">
+    <h1>Register~</h1>
+    <form class="register-form" method="post">
+        <div class="input-cont">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div class="input-cont">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="pwd" required>
+        </div>
+        <div class="submit-btn">
+            <button type="submit">Register</button>
+        </div>
+    </form>
+    <nav class="page-links">
+        <a href="/">Return to Home</a>
+        <a href="/login">Login</a>
+    </nav>
+    <?php require ROOT . 'views/partials/footer.php' ?>
