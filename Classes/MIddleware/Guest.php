7cd5aee17817ba02cc2d7cf29fe99f0332729920
Haemoffy <fuwansuu@gmail.com>
2024-11-22 23:30:05 +0800

    user to auth_user

--- Classes/MIddleware/Guest.php
+++ Classes/MIddleware/Guest.php
@@ -5,7 +5,7 @@ namespace Classes\Middleware;
 class Guest
 {
     public static function handle() {
-        if(isset($_SESSION['user'])) {
+        if(isset($_SESSION['auth_user'])) {
             redirect('/');
         }
     }
