59ed63c018569f397cd3de6c0b7712daa10e04f3
Haemoffy <fuwansuu@gmail.com>
2024-11-22 23:30:57 +0800

    new vars for displaying errors

--- controllers/register/create.php
+++ controllers/register/create.php
@@ -1,7 +1,11 @@
 <?php
 
+use Classes\Session;
+
 return view('register/create.views.php', [
     'title' => "NightShade | Register",
     'bg' => 'register-bg',
-    'bodyLayout' => 'center-body-layout-pos'
+    'bodyLayout' => 'center-body-layout-pos',
+    'emailError' => [Session::get('email') ? 'emailError' : '', Session::get('email')],
+    'passwordError' => [Session::get('password') ? 'passwordError' : '', Session::get('password')]
 ]);
