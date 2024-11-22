dbd0e200909e8f7b6ce02ee105d16d5c27cf4bd3
Haemoffy <fuwansuu@gmail.com>
2024-11-22 23:28:38 +0800

    new routes for dealing with post register & login, todo page, logout

--- routes.php
+++ routes.php
@@ -4,6 +4,14 @@ $router->get('/', 'index.php');
 
 //* Login
 $router->get('/login', 'login/create.php')->only('guest');
+$router->post('/login', 'login/store.php');
 
-//*Register
+//* Register
 $router->get('/register', 'register/create.php')->only('guest');
+$router->post('/register', 'register/store.php');
+
+//* Logout
+$router->delete('/logout', 'logout.php')->only('auth');
+
+//* ToDo
+$router->get('/todo', 'todo/index.php')->only('auth');
