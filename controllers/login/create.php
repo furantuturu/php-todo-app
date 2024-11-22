f5d641d0435d02d110df2366cdbf2660a8c5e9bc
Haemoffy <fuwansuu@gmail.com>
2024-11-22 23:30:34 +0800

    added new var for displaying success and errors

--- controllers/login/create.php
+++ controllers/login/create.php
@@ -1,7 +1,12 @@
 <?php
 
+use Classes\Session;
+
 return view('login/create.views.php', [
     'title' => "NightShade | Login",
     'bg' => 'login-bg',
-    'bodyLayout' => 'center-body-layout-pos'
+    'bodyLayout' => 'center-body-layout-pos',
+    'registerSuccess' => Session::get('success'),    
+    'emailError' => [Session::get('email') ? 'emailError' : '', Session::get('email')],
+    'passwordError' => [Session::get('password') ? 'passwordError' : '', Session::get('password')]
 ]);
