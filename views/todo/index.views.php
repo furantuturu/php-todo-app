f811f9f08a09d8eae7ed37ceac651a71150bd395
Furantuturu <fuwansuu@gmail.com>
2024-11-29 16:31:32 +0800

    init content for todo page

--- views/todo/index.views.php
+++ views/todo/index.views.php
@@ -1,5 +1,11 @@
 <?php require ROOT . 'views/partials/head.php' ?>
-<nav>
-
+<nav class="todo-nav home-bg bg-design">
+    <a href="/" class="home-link">
+        <h1 class="title todo-nav-title">NightShade~</h1>
+    </a>
 </nav>
+<form action="/logout" method="post">
+    <input type="hidden" name="_method" value="DELETE">
+    <button type="submit">Logout</button>
+</form>
 <?php require ROOT . 'views/partials/footer.php' ?>
