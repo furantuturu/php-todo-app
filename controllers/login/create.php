744e5f96eb479661720353c136a59d6e3f0af8bb
Furantuturu <fuwansuu@gmail.com>
2024-11-29 16:29:59 +0800

    removed whitespaces

--- controllers/login/create.php
+++ controllers/login/create.php
@@ -6,7 +6,7 @@ return view('login/create.views.php', [
     'title' => "NightShade | Login",
     'bg' => 'login-bg',
     'bodyLayout' => 'center-body-layout-pos',
-    'registerSuccess' => Session::get('success'),    
+    'registerSuccess' => Session::get('success'),
     'emailError' => [Session::get('email') ? 'emailError' : '', Session::get('email')],
     'passwordError' => [Session::get('password') ? 'passwordError' : '', Session::get('password')]
 ]);
