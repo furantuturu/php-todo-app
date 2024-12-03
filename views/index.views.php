55a59f9e38193b2eacae4386bc7cc8fa4bca2efd
Haemoffy <fuwansuu@gmail.com>
2024-12-03 22:25:33 +0800

    replaced login register button if user is authenticated

--- views/index.views.php
+++ views/index.views.php
@@ -3,8 +3,12 @@
     <h1 class="title">NightShade~</h1>
     <p class="desc">A simple ToDo website for casual ToDo enjoyers.</p>
     <div class="user-admission-btns-cont">
-        <a href="login" class="login-btn" type="button">Login</a>
+        <?php if(isset($_SESSION['auth_user'])): ?>
+        <a href="/todo" class="login-btn" type="button">Go make ToDos!</a>
+        <?php else: ?>
+        <a href="/login" class="login-btn" type="button">Login</a>
         <a href="/register" class="register-btn" type="button">Register</a>
+        <?php endif; ?>
     </div>
 </div>
 <?php require 'partials/footer.php' ?>
