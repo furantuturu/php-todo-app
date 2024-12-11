8396baaf68048c1c8f7f48f875c4884bbb048905
Haemoffy <fuwansuu@gmail.com>
2024-12-11 18:38:40 +0800

    added function for changing todo bg with colorpicker

--- public/static/js/todoPage.js
+++ public/static/js/todoPage.js
@@ -1,6 +1,8 @@
 const avatarDDBtn = document.querySelector('.avatar-dropdown')
 const addTodoBtn = document.querySelector('.add-todo-btn')
 const todoBlock = document.querySelector('.todo-block')
+const colorPicker = document.querySelector('#colorpicker')
+const todoBlocks = document.querySelectorAll('.todo-block')
 
 function openAvatarDropdown() {
     const DDContent = document.querySelector('.dropdown-content')
@@ -19,5 +21,12 @@ function addTodoForm() {
     dialog.showModal()
 }
 
+function todoColorBgChange() {
+    todoBlocks.forEach(block => {
+        block.style.backgroundColor = colorPicker.value
+    })
+}
+
 avatarDDBtn.addEventListener('click', openAvatarDropdown)
 addTodoBtn.addEventListener('click', addTodoForm)
+colorPicker.addEventListener('change', todoColorBgChange)
