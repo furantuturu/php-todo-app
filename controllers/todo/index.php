fa92881dab71a1707ed10a230a306e1eb142ae61
Haemoffy <fuwansuu@gmail.com>
2024-12-16 23:24:26 +0800

    added new variable for checking title errors

--- controllers/todo/index.php
+++ controllers/todo/index.php
@@ -1,7 +1,10 @@
 <?php
 
+use Classes\Session;
+
 return view('todo/index.views.php', [
     'title' => 'NightShade | ToDo',
     'bg' => 'todo-bg',
-    'bodyLayout' => ''
+    'bodyLayout' => '',
+    'titleError' => [Session::get('title') ? 'titleError' : '', Session::get('title')]
 ]);
