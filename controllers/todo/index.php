63fe90bb5577c1021abee48440f59ef40e760d3d
furantuturu <fuwansuu@gmail.com>
2024-12-19 00:08:51 +0800

    variable to apply check-todo class if todo is checked

--- controllers/todo/index.php
+++ controllers/todo/index.php
@@ -3,12 +3,14 @@
 use Classes\Session;
 
 $db = getDatabaseClass();
-$todos = $db->query("SELECT todo_id, title, description FROM todos")->getAll();
+$todos = $db->query("SELECT todo_id, title, description, checked FROM todos")->getAll();
+$checked = filter_var($todos[0]['checked'], FILTER_VALIDATE_BOOLEAN);
 
 return view('todo/index.views.php', [
     'title' => 'NightShade | ToDo',
     'bg' => 'todo-bg',
     'bodyLayout' => '',
     'titleError' => [Session::get('title') ? 'titleError' : '', Session::get('title')],
-    'todos' => $todos
+    'todos' => $todos,
+    'checked' => $checked ? 'checked-todo' : ''
 ]);
