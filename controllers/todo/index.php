378d2aeb138db0ce57a0b8312450c211458231af
furantuturu <fuwansuu@gmail.com>
2024-12-18 11:16:08 +0800

    added variable to assign all fetched todos

--- controllers/todo/index.php
+++ controllers/todo/index.php
@@ -2,9 +2,13 @@
 
 use Classes\Session;
 
+$db = getDatabaseClass();
+$todos = $db->query("SELECT todo_id, title, description FROM todos")->getAll();
+
 return view('todo/index.views.php', [
     'title' => 'NightShade | ToDo',
     'bg' => 'todo-bg',
     'bodyLayout' => '',
-    'titleError' => [Session::get('title') ? 'titleError' : '', Session::get('title')]
+    'titleError' => [Session::get('title') ? 'titleError' : '', Session::get('title')],
+    'todos' => $todos
 ]);
